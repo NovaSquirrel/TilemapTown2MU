f03446c14bd2695af04a9323ceda99d4cba71760
NovaSquirrel <bushytail@novasquirrel.com>
2024-02-24 23:40:32 -0500

    Proof of concept

---
+++ gateway.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+# https://www.moo.mud.org/mcp/
+# https://wiki.mudlet.org/w/Standards:MUD_Client_Media_Protocol
+import asyncio, sys, weakref, signal
+
+# Set up an event loop first
+from twisted.internet import asyncioreactor
+event_loop = asyncio.SelectorEventLoop()
+asyncioreactor.install(event_loop)
+
+# Set up other stuff
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import task
+from twisted.internet import reactor
+from town import *
+
+connect_message = [
+"  _______ _ _                              _______                  ",
+" |__   __(_) |                            |__   __|                 ",
+"    | |   _| | ___ _ __ ___   __ _ _ __      | | _____      ___ __  ",
+"    | |  | | |/ _ \\ '_ ` _ \\ / _` | '_ \\     | |/ _ \\ \\ /\\ / / '_ \\ ",
+"    | |  | | |  __/ | | | | | (_| | |_) |    | | (_) \\ V  V /| | | |",
+"    |_|  |_|_|\\___|_| |_| |_|\\__,_| .__/     |_|\\___/ \\_/\\_/ |_| |_|",
+"                                  | |                               ",
+"                                  |_|     Tilemap Town MU* Gateway! ",
+"  https://github.com/NovaSquirrel/TilemapTown2MU",
+"  https://novasquirrel.com/town/",
+" ----------------------------------------------",
+"  'connect <character> <password>' to connect",
+"  'connect guest guest' to connect as a guest",
+"  QUIT to disconnect"
+]
+#tilemap_town_uri = 'ws://localhost:12550'
+tilemap_town_uri = 'wss://novasquirrel.com/townws/'
+
+# ---------------------------------------------------------
+
+def separate_first_word(text, lowercase_first=True):
+	space = text.find(" ")
+	command = text
+	arg = ""
+	if space >= 0:
+		command = text[0:space]
+		arg = text[space+1:]
+	if lowercase_first:
+		command = command.lower()
+	return (command, arg)
+
+# ---------------------------------------------------------
+
+class MUConnection(LineReceiver): # Handles a single user's connection
+	def __init__(self):
+		self.line_handler = self.pre_connect_state_handler
+		self.idle_time = 0
+		self.tilemap_town = None
+
+	# Handles string->bytes conversion for sendLine
+	def sendLineAsBytes(self, text):
+		# uncomment the next two lines and the Timer import and comment out the normal sendline to introduce 1s of latency to all messages for lag testing
+		#t = Timer(1, self.sendLine, args=[text.encode('utf-8')])
+		#t.start()
+		self.sendLine(text.encode('utf-8'))
+
+	def sendLinesAsBytes(self, lines):
+		for line in lines:
+			self.sendLine(line.encode('utf-8'))
+
+	# .----------------------------------------------------
+	# | Callbacks
+	# '----------------------------------------------------
+
+	def connectionMade(self):
+		self.transport.setTcpNoDelay(True)
+		self.ip = self.transport.getPeer().host
+		self.sendLinesAsBytes(connect_message)
+		print("Connection from " + self.ip)
+
+	def connectionLost(self, reason):
+		print("Client disconnected: %s" % reason)
+		if self.tilemap_town and self.tilemap_town.websocket:
+			asyncio.ensure_future(self.tilemap_town.websocket.close(), loop=event_loop)
+			
+	def lineReceived(self, line):
+		self.idle_time = 0
+		try:
+			self.line_handler(line.decode('utf-8'))
+		except:
+			exception_type = sys.exc_info()[0]
+			print("Exception: %s" % exception_type.__name__)
+			self.sendLineAsBytes("Error: Exception (%s)" % exception_type.__name__)
+			raise
+
+	# .----------------------------------------------------
+	# | Utilities
+	# '----------------------------------------------------
+
+	def disconnect(self, reason):
+		if reason != None:
+			self.sendLineAsBytes(reason)
+		self.transport.loseConnection()
+
+	def send_to_town(self, command, params):
+		if self.tilemap_town:
+			self.tilemap_town.send_command(command, params)
+
+	# .----------------------------------------------------
+	# | States
+	# '----------------------------------------------------
+
+	def pre_connect_state_handler(self, text):
+		command, arg = separate_first_word(text)
+		if command == "connect": # connect username password
+			username, password = separate_first_word(arg)
+			self.tilemap_town = TilemapTown(self)
+			self.line_handler = self.connecting_state_handler
+			asyncio.ensure_future(self.tilemap_town.run_client(tilemap_town_uri, username, password), loop=event_loop)
+			self.sendLineAsBytes("Connecting to Tilemap Town...")
+		elif command == "test":
+			self.sendLineAsBytes("Test")
+		elif command == "quit":
+			self.disconnect("Goodbye!")
+
+	def connecting_state_handler(self, text):
+		pass
+
+	def connected_state_handler(self, text):
+		# Look for a prefix like " or :
+		for k, v in prefix_gateway_command_handlers.items():
+			if text.startswith(k):
+				v(self, text[len(k):])
+				return
+
+		# Otherwise it's a normal command
+		command, arg = separate_first_word(text)
+		if command in gateway_command_handlers:
+			gateway_command_handlers[command](self, arg)
+		else:
+			self.sendLineAsBytes("Error: Unknown gateway command: %s" % command)
+
+# ---------------------------------------------------------
+
+class MUFactory(Factory):
+	def __init__(self):
+		self.users = weakref.WeakSet()
+		signal.signal(signal.SIGINT, self.signal_handler)
+
+	def buildProtocol(self, addr):
+		connection = MUConnection()
+		self.users.add(connection)
+		return connection
+	
+	def signal_handler(self, sig, frame):
+		# Let everyone know they were disconnected for maintenance
+		for user in self.users:
+			user.disconnect("Gateway server shutting down")
+		reactor.stop()
+
+def check_on_timeouts():
+	for user in factory.users:
+		user.idle_time += 1
+
+# .----------------------------------------------
+# | Command registration
+# '----------------------------------------------
+
+gateway_command_handlers = {}
+prefix_gateway_command_handlers = {}
+
+def gateway_command(prefix=None):
+	def decorator(f):
+		if prefix != None:
+			prefix_gateway_command_handlers[prefix] = f
+		command_name = f.__name__[3:]
+		gateway_command_handlers[command_name] = f
+	return decorator
+
+# .----------------------------------------------
+# | Gateway commands
+# '----------------------------------------------
+
+@gateway_command()
+def fn_echo(self, arg):
+	self.sendLineAsBytes(arg)
+
+@gateway_command(prefix='"')
+def fn_say(self, arg):
+	self.send_to_town("MSG", {"text": arg})
+
+@gateway_command(prefix=':')
+def fn_pose(self, arg):
+	self.send_to_town("MSG", {"text": "/me "+arg})
+
+@gateway_command()
+def fn_spoof(self, arg):
+	self.send_to_town("MSG", {"text": "/spoof "+arg})
+
+@gateway_command(prefix='/')
+def fn_cmd(self, arg):
+	if arg.startswith("me ") or arg.startswith("spoof ") or arg.startswith("ooc "):
+		self.send_to_town("MSG", {"text": "/"+arg})
+	else:
+		self.send_to_town("CMD", {"text": arg})
+
+@gateway_command()
+def fn_allcommands(self, arg):
+	self.sendLineAsBytes(" ".join(gateway_command_handlers.keys()))
+
+def try_edge_link(self, link):
+	if self.tilemap_town and self.tilemap_town.map_info:
+		info = self.tilemap_town.map_info
+		if "edge_links" in info:
+			if info["edge_links"][link]:
+				self.send_to_town("CMD", {"text": "map %s" % info["edge_links"][link]})
+			else:
+				self.sendLineAsBytes("That map isn't connected in that direction")
+		else:
+			self.sendLineAsBytes("That map isn't connected to other maps")
+
+@gateway_command()
+def fn_east(self, arg):
+	try_edge_link(self, 0)
+@gateway_command()
+def fn_southeast(self, arg):
+	try_edge_link(self, 1)
+@gateway_command()
+def fn_south(self, arg):
+	try_edge_link(self, 2)
+@gateway_command()
+def fn_southwest(self, arg):
+	try_edge_link(self, 3)
+@gateway_command()
+def fn_west(self, arg):
+	try_edge_link(self, 4)
+@gateway_command()
+def fn_northwest(self, arg):
+	try_edge_link(self, 5)
+@gateway_command()
+def fn_north(self, arg):
+	try_edge_link(self, 6)
+@gateway_command()
+def fn_northeast(self, arg):
+	try_edge_link(self, 7)
+
+# .----------------------------------------------
+# | Server setup
+# '----------------------------------------------
+
+print("Ready for clients")
+factory = MUFactory()
+reactor.listenTCP(4201, factory)
+reactor.listenTCP(4201, factory, interface="::")
+l = task.LoopingCall(check_on_timeouts)
+l.start(30) # Every 30 seconds
+reactor.run()
---
+++ town.py
@@ -0,0 +1,246 @@
+# Tilemap Town connection
+from shared import *
+import asyncio, json, websockets
+
+# .----------------------------------------------
+# | Tilemap Town class
+# '----------------------------------------------
+
+class TilemapTown(object):
+	def __init__(self, connection):
+		self.entity_id = None
+		self.websocket = None
+		self.mu_connection = connection
+
+		# State
+		self.entity_id = None
+		self.map_id = None
+		self.map_info = {}
+		self.who = {}
+
+	# .----------------------------------------------
+	# | Websocket connection
+	# '----------------------------------------------
+
+	async def run_client(self, uri, username, password):
+		print("Connecting")
+
+		async with websockets.connect(uri=uri, extra_headers={'X-Real-IP': 'testing'}) as self.websocket:
+			try:
+				idn_info = {
+					#"name": "Gateway test",
+					"client_name": "TilemaptTown2MU",
+					"features": {
+						"batch": {"version": "0.0.1"}
+					}
+				}
+				if username != None and username != 'guest':
+					idn_info["username"] = username
+					idn_info["password"] = password
+				self.send_command("IDN", idn_info)
+				async for message in self.websocket:
+					# Split the message into parts
+					if len(message)<3:
+						continue
+					if message[0:4] == "BAT ":
+						for sub_message in message[4:].splitlines():
+							self.receive_server_message(sub_message)
+					else:
+						self.receive_server_message(message)
+
+			except websockets.ConnectionClosed:
+				print("Connection closed")
+
+	def receive_server_message(self, message):
+		print("<< "+message)
+		message_type = message[0:3]
+		arg = {}
+		if len(message) > 4:
+			arg = json.loads(message[4:])
+		if message_type in protocol_handlers:
+			protocol_handlers[message_type](self, arg)
+
+	def send_command(self, command, params):
+		print(">> "+command+ " " + (json.dumps(params) if params != None else ""))
+		if self.websocket == None:
+			return
+		def make_protocol_message_string(command, params):
+			if params != None:
+				return command + " " + json.dumps(params)
+			return command
+		asyncio.ensure_future(self.websocket.send(make_protocol_message_string(command, params)))
+
+	def send_cmd_command(self, command):
+		self.send_command("CMD", {"text": command})
+
+	def print_line(self, text):
+		self.mu_connection.sendLineAsBytes(text)
+
+	def print_lines(self, text):
+		self.mu_connection.sendLinesAsBytes(text)
+
+# -----------------------------------------------------------------------------
+
+# .----------------------------------------------
+# | Command registration
+# '----------------------------------------------
+
+protocol_handlers = {}
+def protocol_command():
+	def decorator(f):
+		command_name = f.__name__[3:]
+		protocol_handlers[command_name] = f
+	return decorator
+
+# .----------------------------------------------
+# | Protocol message handlers
+# '----------------------------------------------
+
+@protocol_command()
+def fn_PIN(self, arg):
+	self.send_command("PIN", None)
+
+@protocol_command()
+def fn_ERR(self, arg):
+	self.print_line("Error: " + arg["text"])
+
+@protocol_command()
+def fn_CMD(self, arg):
+	self.print_line(">> " + arg["text"])
+
+@protocol_command()
+def fn_MSG(self, arg):
+	if "name" in arg:
+		lowercase = arg["text"].lower()
+		if lowercase.startswith("/me "):
+			self.print_line("* %s %s" % (arg["name"], arg["text"][4:]))
+		elif lowercase.startswith("/ooc "):
+			self.print_line("[OOC] %s: %s" % (arg["name"], arg["text"][5:]))
+		elif lowercase.startswith("/spoof "):
+			self.print_line("* %s (by %s)" % (arg["text"][7:], arg["name"]))
+		else:
+			self.print_line("<%s> %s" % (arg["name"], arg["text"]))
+	else:
+		self.print_line("Server message: %s" % arg["text"])
+		if "buttons" in arg:
+			self.print_line("[Buttons]")
+			for i in range(len(arg["buttons"]) // 2):
+				option  = arg["buttons"][i*2]
+				command = arg["buttons"][i*2+1]
+				self.print_line("| %s: %s" % (option, command))
+
+@protocol_command()
+def fn_PRI(self, arg):
+	text = arg["text"]
+	lower = text.lower()
+	if lower.startswith("/me "):
+		text = text[4:]
+		if arg["receive"]:
+			self.print_line("In a page-pose to you, %s %s." % (arg["name"], text))
+		else:
+			self.print_line("You page-pose, \"%s\" to %s." % (text, arg["name"]))
+	elif lower.startswith("/ooc "):
+		text = text[5:]
+		if arg["receive"]:
+			self.print_line("%s pages, \"[OOC] %s\" to you." % (arg["name"], text))
+		else:
+			self.print_line("You page, \"[OOC] %s\" to %s." % (text, arg["name"]))
+	else:
+		if arg["receive"]:
+			self.print_line("%s pages, \"%s\" to you." % (arg["name"], arg["text"]))
+		else:
+			self.print_line("You page, \"%s\" to %s." % (arg["text"], arg["name"]))
+
+@protocol_command()
+def fn_BAG(self, arg):
+	pass
+
+@protocol_command()
+def fn_WHO(self, arg):
+	if "you" in arg:
+		self.entity_id = arg["you"]
+
+	if "new_id" in arg:
+		i = arg["new_id"]
+		if i["id"] not in self.who:
+			return
+		self.who[i["new_id"]] = self.who[i["id"]]
+		del i["id"]
+		if i["id"] == self.entity_id:
+			self.entity_id = i["new_id"]
+
+	if "list" in arg:
+		self.who = arg['list']
+
+	if "add" in arg:
+		i = arg["add"]
+		_id = i["id"]
+		if _id not in self.who:
+			self.print_line("Joining: %s" % (self.who[i].get("name", str(i))))
+		self.who[_id] = i
+
+	if "remove" in arg and arg["remove"] in self.who:
+		i = arg["remove"]
+		self.print_line("Leaving: %s" % (self.who[i].get("name", str(i))))
+		del self.who[i]
+
+	if "update" in arg:
+		i = arg["update"]
+		_id = i["id"]
+		if _id not in self.who:
+			return
+		if ("status" in i and i["status"] != self.who[_id].get("status", None)) or \
+		("status" in i and i["status_message"] != self.who[_id].get("status_message", None)):
+			if i["status"]:
+				status_lower = status_name.lower()
+				if status_lower == "away":
+					status_name = "away"
+				elif status_lower == "busy":
+					status_name = "busy"
+				elif status_lower == "ic":
+					status_name = "in-character"
+				elif status_lower == "iic":
+					status_name = "looking to be in-character"
+				elif status_lower == "ooc":
+					status_name = "out of character"
+				else:
+					status_name = "'" + i["status"] + "'"
+				line = "! %s set their status to %s" % (self.who[_id].name, status_name)
+				if i["status_message"]:
+					line += " (%s)" % i["status_message"]
+				self.print_line(line)
+			else:
+				self.print_line("! %s cleared their status" % self.who[_id].name)
+		self.who[_id].update(i)
+
+@protocol_command()
+def fn_IDN(self, arg):
+	if self.mu_connection:
+		self.print_line("Connected!")
+		self.mu_connection.line_handler = self.mu_connection.connected_state_handler
+
+@protocol_command()
+def fn_MAI(self, arg):
+	if "remote_map" in arg:
+		return # Shouldn't be receiving these anyway
+	self.map_id = arg["id"]
+	self.map_info = arg
+
+	self.print_line("Now entering: %s" % arg["name"])
+	if "desc" in arg and arg["desc"]:
+		self.print_line("  "+arg["desc"])
+	if "edge_links" in arg:
+		edge_links = arg["edge_links"]
+		link_names = []
+		for i in range(8):
+			if edge_links[i]:
+				link_names.append(["East", "SouthEast", "South", "SouthWest", "West", "NorthWest", "North", "NorthEast"][i])
+		if len(link_names):
+			self.print_line("Obvious Exits:")
+			self.print_line("  %s" % ", ".join(link_names))
+	if len(self.who) > 1:
+		people = []
+		for k,v in self.who.items():
+			if v.get("name", None):
+				people.append(v["name"])
+		self.print_line("Contents: %s" % ", ".join(people))
